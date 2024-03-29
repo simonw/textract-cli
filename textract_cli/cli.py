43d385b375bb76b91b539db4eb0f5da884cd42b3
Simon Willison <swillison@gmail.com>
2024-03-29 10:32:37 -0700

    First working prototype (by GPT-4) refs #1

--- textract_cli/cli.py
+++ textract_cli/cli.py
@@ -1,21 +1,32 @@
+import boto3
 import click
 
 
-@click.group()
+@click.command()
 @click.version_option()
-def cli():
-    "CLI for running files through AWS Textract"
-
-
-@cli.command(name="command")
-@click.argument(
-    "example"
-)
+@click.argument("image_path", type=click.Path(exists=True))
 @click.option(
-    "-o",
-    "--option",
-    help="An example option",
+    "-o", "--output", type=click.File("w"), help="Output file to write the results to"
 )
-def first_command(example, option):
-    "Command description goes here"
-    click.echo("Here is some output")
+def cli(image_path, output):
+    """CLI tool to extract text from an image using AWS Textract."""
+    # Create a Textract client
+    client = boto3.client("textract")
+
+    # Read the image as bytes
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+
+    # Call the detect_document_text API
+    response = client.detect_document_text(Document={"Bytes": image_bytes})
+
+    # Collect the detected text
+    detected_text = "\n".join(
+        item["Text"] for item in response["Blocks"] if item["BlockType"] == "LINE"
+    )
+
+    # Output the results
+    if output:
+        output.write(detected_text)
+    else:
+        click.echo(detected_text)
